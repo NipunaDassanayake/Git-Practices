7ff4542b7c081c1358c940163de6a3bfa2fa0217
NipunaDassanayake <Nipuna920@gmail.com>
2026-01-07 14:05:06 +0530

    chore: update price data type

--- src/main/java/com/example/Git_Practices/model/Product.java
+++ src/main/java/com/example/Git_Practices/model/Product.java
@@ -13,5 +13,5 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private Double price;
+    private Integer price;
 }
