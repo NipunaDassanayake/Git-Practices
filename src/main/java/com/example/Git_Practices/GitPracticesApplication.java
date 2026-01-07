cb3ab49df49ef8ddb0966e00a3eb302075ae83b9
NipunaDassanayake <Nipuna920@gmail.com>
2026-01-07 12:58:14 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/Git_Practices/GitPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Git_Practices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPracticesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Git_Practices/GitPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Git_Practices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
