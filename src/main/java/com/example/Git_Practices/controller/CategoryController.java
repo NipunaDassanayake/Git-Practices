52113c8127d1606718bf0ea3b4ad96307782a5cb
NipunaDassanayake <Nipuna920@gmail.com>
2026-01-08 12:25:45 +0530

    feat: implement category domain layers

--- src/main/java/com/example/Git_Practices/controller/CategoryController.java
+++ src/main/java/com/example/Git_Practices/controller/CategoryController.java
@@ -1,27 +1,30 @@
 package com.example.Git_Practices.controller;
+import com.example.Git_Practices.controller.dto.request.CategoryRequestDTO;
 import com.example.Git_Practices.model.Category;
 import com.example.Git_Practices.repository.CategoryRepository;
+import com.example.Git_Practices.service.CategoryService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
+@AllArgsConstructor
 public class CategoryController {
 
-    @Autowired
-    private CategoryRepository categoryRepository;
+    private final CategoryService categoryService;
 
-    // Get all categories
-    @GetMapping
-    public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody CategoryRequestDTO requestDTO) {
+        return new ResponseEntity<>(categoryService.createCategory(requestDTO), HttpStatus.CREATED);
     }
 
-    // Create a new category
-    @PostMapping
-    public Category createCategory(@RequestBody Category category) {
-        return categoryRepository.save(category);
+    @GetMapping
+    public ResponseEntity<List<Category>> getAll() {
+        return ResponseEntity.ok(categoryService.getAllCategories());
     }
 }
